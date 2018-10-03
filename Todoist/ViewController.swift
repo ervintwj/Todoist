9004a2322eb1ae2e5a2e55086acfb5b01fa0bebe
Ervin <ervintwj@gmail.com>
2018-10-03 23:56:33 +0800

    Initial Commit

---
+++ Todoist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoist
+//
+//  Created by Ervin on 3/10/18.
+//  Copyright © 2018 Ervin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
