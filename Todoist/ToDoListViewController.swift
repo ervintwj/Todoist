079b97f160a6a60d83b509f0a8ec8d582642bf43
Ervin <ervintwj@gmail.com>
2018-10-04 00:39:42 +0800

    To-do List completed with Data Source and Delegate methods.

---
+++ Todoist/ToDoListViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  Todoist
+//
+//  Created by Ervin on 3/10/18.
+//  Copyright © 2018 Ervin. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+    
+    let itemArray = ["Purchase iPhone XS", "Figure out Core Data", "Watch Google's Event"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+//MARK: - TableView DataSource
+extension ToDoListViewController {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell")!
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    
+}
+
+//MARK: - TableView Delegate Interactions
+extension ToDoListViewController {
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType != .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+}
